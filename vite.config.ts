fcf100c5b65858a01871312fcfe4581ab66878f2
Alina Ovod <alina.ipatova98@gmail.com>
2025-03-16 22:48:39 +0200

    deploy using GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/recipe-book/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
